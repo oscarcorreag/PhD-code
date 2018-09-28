0a11feca0d7efb268816513d40dba7032413a98e
Oscar Correa <oscarcorreag@gmail.com>
2018-09-28 21:52:34 +1000

    Test

--- VST-RS/dreyfus_based_wc.py
+++ VST-RS/dreyfus_based_wc.py
@@ -4,4 +4,5 @@ class DreyfusBasedWC:
         self.__graph = graph.copy()
         self.__graph.compute_dist_paths(compute_paths=False)
 
-    # def steiner_forest(self, users, pois, z):
+    def steiner_forest(self, users, pois, z):
+        return True
