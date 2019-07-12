368d703a1ea1e848ffd6b4d2bf65f7b684d57ee7
Oscar Correa <oscarcorreag@gmail.com>
2019-07-12 14:56:55 +1000

    let see

--- Pickup-Delivery/test/MILP_v_SP-based_experiments.py
+++ Pickup-Delivery/test/MILP_v_SP-based_experiments.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     num_req_per_retailer = 4
     num_drv_per_retailer = 2
     #
-    results = [] 
+    results = []
     sample = 0
     seed = 10434
     for region, info in regions.iteritems():
